d02f78f1ee19f0aae216004c954e3a6e91177e6d
Justin Bengtson <36495451+JB184351@users.noreply.github.com>
2022-09-05 19:20:27 -0600

    Delete SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa directory
    Removing more files...

--- SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/Extensions/CodableBundleExtension.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CodableBundleExtension.swift
-//  Africa
-//
-//  Created by Justin Bengtson on 2/19/22.
-//
-
-import Foundation
--- SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/Models/CoverImageModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CoverImageModel.swift
-//  Africa
-//
-//  Created by Justin Bengtson on 2/19/22.
-//
-
-import Foundation
